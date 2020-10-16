0168e024990ddc1011fa646767e758cc024cacf2
David Nieto naranjo <davidnieto157@gmail.com>
2020-10-16 10:05:50 +0200

    Menu dentro de menu

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import { menu } from '../views/menu'
+import { menu, menutriangulo } from '../views/menu'
 import { leerTeclado } from '../views/lecturaTeclado'
 import { Triangulo } from '../figuras/triangulo'
 import { Cuadrado } from '../figuras/cuadrado'
@@ -61,6 +61,50 @@ const main = async () => {
                 
                 console.log("Perímetro del círculo="+circulo.perimetro())
                 console.log("Área del círculo="+circulo.area())
+                break
+            case 5:
+                    let m: number
+                    do {
+                        m = await menutriangulo()
+                        switch(m){
+                            case 1:
+                                console.log('Perimetro del triangulo............')
+                
+                                let base2, ladoa2, ladob2: number
+                
+                                base2 =  parseInt( await leerTeclado('Base del triángulo:'))
+                                ladoa2 =  parseInt( await leerTeclado('Lado A del triángulo:'))
+                                ladob2 =  parseInt( await leerTeclado('Lado B del triángulo:'))
+                
+                                let triangulo2 = new Triangulo(base2,ladoa2,ladob2)
+                
+                                console.log("Perímetro del triángulo="+triangulo2.perimetro())
+                                
+                                break
+                            case 2:
+                                console.log('Area del triangulo............')
+
+                                let base3, altura3, ladoa3, ladob3: number
+
+                                base3 =  parseInt( await leerTeclado('Base del triángulo:'))
+                                altura3 =  parseInt( await leerTeclado('Altura del triángulo:'))
+                                ladoa3 =  parseInt( await leerTeclado('Lado A del triángulo:'))
+                                ladob3 =  parseInt( await leerTeclado('Lado B del triángulo:'))
+
+                                let triangulo3 = new Triangulo(base3,altura3,ladoa3,ladob3)
+
+                                console.log("Área del triángulo="+triangulo3.area())
+
+                                break
+                            case 0:
+                                console.log('\nGracias por su participación')
+                                break
+                            default:
+                                console.log("Opción incorrecta")
+                                break
+                        }
+                    }while (m != 0)
+                main
                 break
             case 0:
                 console.log('\nGracias por su participación')
--- views/menu.ts
+++ views/menu.ts
@@ -7,7 +7,18 @@ export const menu = async () => {
     console.log('2.- Cuadrado')
     console.log('3.- Circulo')
     console.log('4.- Rectangulo')
+    console.log('5.- Triangulo 2')
     console.log('0.- SALIR')
     n = parseInt( await leerTeclado('opción: ') )
     return n
+}
+
+export const menutriangulo = async () => {
+    let m: number
+    console.log('\nElija una opción:')
+    console.log('1.- Perimetro')
+    console.log('2.- Area')
+    console.log('0.- SALIR')
+    m = parseInt( await leerTeclado('opción: ') )
+    return m
 }
